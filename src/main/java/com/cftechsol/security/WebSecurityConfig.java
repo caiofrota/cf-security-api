e236b9654da02a0190b9eb2bca20e3a5e6b9f313
Caio Frota <caiofrota@gmail.com>
2018-04-16 16:34:38 -0300

    Javadoc fix.

--- src/main/java/com/cftechsol/security/WebSecurityConfig.java
+++ src/main/java/com/cftechsol/security/WebSecurityConfig.java
@@ -26,12 +26,12 @@ import com.cftechsol.security.jwt.JWTLoginFilter;
  * 
  * {@literal <propertie> : <default value>}
  * 
- * @formatter:off
+ * {@literal @formatter:off}
  * cf.security.query.users_by_username : select username, password, enabled from users where username = ?
  * cf.security.query.authorities_by_username : select username, authority from authorities where username = ?
  * cf.security.path.login : /login
  * cf.security.path.secured : /admin
- * @formatter:on
+ * {@literal @formatter:on}
  * 
  * @author Caio Frota {@literal <contact@cftechsol.com>}
  * @version 1.0.0
