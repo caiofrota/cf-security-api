d032c9d2b38bf40b107349b90fa4b546465a7f3f
Caio Frota <caiofrota@gmail.com>
2018-09-11 01:10:32 -0300

    Insert permissions.

---
+++ src/main/resources/db/migration/V20180911004200__insert_permissions.sql
@@ -0,0 +1,24 @@
+-- Insert role permissions
+insert into cf_role_permissions (created_by, created_on, updated_by, updated_on, role_id, permission_id, superadmin)
+                         values (1, now(), 1, now(), 2, 3, 0),
+                                (1, now(), 1, now(), 2, 4, 0),
+                                (1, now(), 1, now(), 2, 5, 0),
+                                (1, now(), 1, now(), 2, 6, 0),
+                                (1, now(), 1, now(), 2, 7, 0),
+                                (1, now(), 1, now(), 2, 8, 0),
+                                (1, now(), 1, now(), 2, 9, 0),
+                                (1, now(), 1, now(), 2, 10, 0),
+                                (1, now(), 1, now(), 2, 11, 0),
+                                (1, now(), 1, now(), 2, 12, 0),
+                                (1, now(), 1, now(), 2, 13, 0),
+                                (1, now(), 1, now(), 2, 14, 0),
+                                (1, now(), 1, now(), 2, 15, 0),
+                                (1, now(), 1, now(), 2, 16, 0),
+                                (1, now(), 1, now(), 2, 17, 0),
+                                (1, now(), 1, now(), 2, 18, 0),
+                                (1, now(), 1, now(), 2, 19, 0),
+                                (1, now(), 1, now(), 2, 20, 0),
+                                (1, now(), 1, now(), 2, 21, 0),
+                                (1, now(), 1, now(), 2, 22, 0),
+                                (1, now(), 1, now(), 2, 23, 0)
+;
